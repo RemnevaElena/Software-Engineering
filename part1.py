02265c62bb93fcb9aa3250a544a8a5c8587bce86
lenok_vse_0k <a-remniva2013@ya.ru>
2022-12-18 11:48:03 +0300

    Read sum in rubles

--- part1.py
+++ part1.py
@@ -1,9 +1,24 @@
+def get_amount(string):
+    ok = True
+    for char in string:
+        if not (char.isdigit() or char == '.'):
+            ok = False
+            break
+    if ok:
+        return float(string)
+    else:
+        return None50
 def main():
     currencies = ["USD", "EUR", "AUD", "CAD", "BYN", "KZT", "UAH", "GBP", "CZK", "CHF", "JPY"]
     info = "Supported currencies:"
     for currency in currencies:
         info += " " + currency
     print(info)
+
+    amount = get_amount(input("Amount in rubles: "))
+    if amount == None:
+        print("Error: Wrong amount.")
+        return
 
 if __name__ == '__main__':
     main()
